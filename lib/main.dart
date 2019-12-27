0505f1cb761343d76ffc62e944e3ac264559406c
常帅 <changshuai@renrenche.com>
2019-12-27 18:01:14 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("MyFlutter"),
         ),
-        body: HomeContent17(),
+        body: HomeContent2(),
       ),
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//Center简易组件
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -33,49 +34,68 @@ class HomeContent extends StatelessWidget {
         textDirection: TextDirection.ltr,
         style: TextStyle(
           fontSize: 40.0,
-          color: Color.fromARGB(100, 100, 100, 255),
+          color: Colors.green,
         ),
       ),
     );
   }
 }
 
+//Container和Text组件2
 class HomeContent2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        //child对其方式为：居中
+        alignment: Alignment.center,
+        //外边距为12
+        margin: EdgeInsets.all(12),
+        //内边距
+        padding: EdgeInsets.fromLTRB(10, 10, 20, 20),
+        //高度
+        height: 300,
+        //宽度
+        width: 300,
+        //配置装饰
+        decoration: BoxDecoration(
+            //容器背景颜色
+            color: Colors.yellow,
+            //设置圆角弧度
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            //设置边框样式
+            border: Border.all(
+                color: Colors.red, //边框颜色
+                width: 2 //边框宽度
+                )),
+        //沿着Z轴旋转0.3
+        transform: Matrix4.rotationZ(0.3),
         child: Text(
-          "内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容",
-          textAlign: TextAlign.left,
-          maxLines: 2,
+          "你好，中国你好，中国你好，中国你好，中国",
+          //文字左对齐
+          textAlign: TextAlign.start,
+          //字体显示倍率：2倍
+          textScaleFactor: 2,
+          //文字最大显示行数为1行
+          maxLines: 1,
+          //文字超出屏幕以后，省略号处理
           overflow: TextOverflow.ellipsis,
           style: TextStyle(
-            color: Colors.white,
-            fontSize: 16.0,
-            fontWeight: FontWeight.w800,
-            fontStyle: FontStyle.italic,
-            decoration: TextDecoration.lineThrough,
-            decorationColor: Colors.black,
-            decorationStyle: TextDecorationStyle.dashed,
-            letterSpacing: 5.0,
-          ),
+              //字体大小10
+              fontSize: 10,
+              //文字装饰线：删除线
+              decoration: TextDecoration.lineThrough,
+              //文字装饰线颜色：红色是
+              decorationColor: Colors.blue,
+              //文字装饰线风格：两根实线
+              decorationStyle: TextDecorationStyle.double,
+              //文字样式:斜体
+              fontStyle: FontStyle.italic,
+              //字体粗细：加粗8倍
+              fontWeight: FontWeight.w800,
+              //文字间距变大5被
+              letterSpacing: 5),
         ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: Colors.red,
-          border: Border.all(color: Colors.blue, width: 2.0),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-//        padding: EdgeInsets.all(10),
-//        padding: EdgeInsets.fromLTRB(0, 10, 20, 30),
-//        transform: Matrix4.translationValues(100, 0, 0)//位移
-//        transform: Matrix4.rotationZ(1),
-        transform: Matrix4.diagonal3Values(1.2, 1, 1),
-        alignment: Alignment.topRight,
       ),
     );
   }
